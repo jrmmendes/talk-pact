06e972b1b647061b9c9d84a34ded5c2a940b929a
Romildo Júnior <jose.romildo.junior@concrete.com.br>
2020-11-18 11:56:48 -0300

    Set jest environment to prevent cors errors with pact

--- consumer/src/api.js
+++ consumer/src/api.js
@@ -4,7 +4,6 @@ const baseUrl = 'http://localhost:3001';
 
 export const getHeroesList = async () => {
   return axios.get(`${baseUrl}/heroes`)
-    .then((res) => res.data)
     .catch((error) => {
       return error.res;
     });
